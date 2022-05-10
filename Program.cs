8afd80c540acd7f59a1aa24843700b4d780900ba
Роман Пенских <romanpenskih@MacBook-Air-Roman.local>
2022-05-10 21:18:36 +0400

    Добавил первый метод FillArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿/* Написать программу, которая . из имеющегося массива строк формирует массив из строк, длинна которых меньше либо равна 3 символа.*/
+string[] array = new string[4];
+
+void FillArray(string[] collection)
+{
+    int length = collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        Console.WriteLine("Задайте данные для массива");
+        collection[index] = Convert.ToString(Console.ReadLine());
+    }
+}
